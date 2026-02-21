5458cd3033af60ccf000de60335dd37a93da81e8
Ewazer <ewazer.com@gmail.com>
2026-02-21 22:28:30 +0100

    Refactor imports and update __all__ in __init__.py

--- chesscore/__init__.py
+++ chesscore/__init__.py
@@ -1,2 +1,8 @@
+from . import constants as constant
+from .constants import *
+from .constants import __all__ as _constants_all
 from .chess_game import *
+from .chess_game import __all__ as _chess_game_all
 from .chess_game import __version__, __author__
+
+__all__ = [*dict.fromkeys([*_chess_game_all, *_constants_all, "constant", "__version__", "__author__"])]
